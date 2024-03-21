7fde745c7ceb09892ed9e5a47d34ce59ba5659eb
Quil180 <quil180gaming@gmail.com>
2024-03-21 02:33:39 -0400

    added in snippet support in nvim

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,13 +1,19 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
     local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.clangd.setup({})
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities
+    })
+    lspconfig.clangd.setup({
+      capabilities = capabilities
+    })
 
     -- keybinds
     vim.keymap.set('n', 'k', vim.lsp.buf.hover, {})
     vim.keymap.set('n', 'd', vim.lsp.buf.definition, {})
-    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
   end
 }
